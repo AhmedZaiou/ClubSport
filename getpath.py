87fae30de185457188b4a032f03f1be50efa29b7
Ahmed Zaiou <ahmedzaiou@MacBook-Air-de-Ahmed-Hanae.local>
2024-12-21 23:10:50 +0100

    wip

---
+++ getpath.py
@@ -0,0 +1,13 @@
+ 
+
+
+from pathlib import Path 
+
+ 
+dataset = Path.home()/"dataset"
+
+
+path_profils_images = Path.home()/"images"/"profiles"
+
+path_data_set = dataset/"royal_fitness.db"
+print(path_data_set)
